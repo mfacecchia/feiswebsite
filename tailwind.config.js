7b4758b5ad3abadc823b988a95015c64ee7c284e
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-10 17:23:14 +0100

    chore: add tailwind/postcss configuration scripts

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const autoprefixer = require('autoprefixer');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./**/.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
+
