4d99fe51cb50f538e83983a5e43d77b4b7a65fe9
Marco Facecchia <facecchiamarco@icloud.com>
2025-01-20 12:35:21 +0100

    fix: theme not being set to OS preference if preference was not set

--- assets/script/themeController.js
+++ assets/script/themeController.js
@@ -1,22 +1,24 @@
-window.matchMedia("(prefers-color-scheme: dark)"),
-    addEventListener("change", (e) => {
-        updateThemePreference();
-    });
-
-function getPreferredTheme() {
-    let preference = localStorage.getItem("theme_preference");
-    if (!preference) {
-        updateThemePreference();
-    }
-    updateThemeControllerCheckbox(preference === "dark");
+function loadPageTheme() {
+    const preference = getThemePreference();
+    updateThemePreference(preference);
     setPageTheme(preference);
 }
 
-function updateThemePreference() {
-    const body = document.querySelector("body");
-    const preference = body.classList.contains("dark") ? "dark" : "light";
-    updateThemeControllerCheckbox(preference === "dark");
-    localStorage.setItem("theme_preference", preference);
+function getThemePreference() {
+    let preference = localStorage.getItem("theme_preference");
+    if (!preference) {
+        preference = window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+    }
+    return preference;
+}
+
+function updateThemePreference(preference) {
+    localStorage.setItem(
+        "theme_preference",
+        preference === "dark" ? "dark" : "light"
+    );
 }
 
 function updateThemeControllerCheckbox(isDark) {
@@ -24,14 +26,18 @@ function updateThemeControllerCheckbox(isDark) {
 }
 
 function setPageTheme(preference) {
-    if (!["light", "dark"].includes(preference) || preference === "light") {
-        // Light theme
-        document.querySelector("body").classList.remove("dark");
+    updateThemeControllerCheckbox(preference === "dark");
+    const body = document.querySelector("body");
+    if (preference === "dark") {
+        // Setting light theme
+        body.classList.add("dark");
         return;
     }
-    document.querySelector("body").classList.add("dark");
+    body.classList.remove("dark");
 }
 
 function toggleTheme() {
-    document.querySelector("body").classList.toggle("dark");
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+    updateThemePreference(body.classList.contains("dark") ? "dark" : "light");
 }
